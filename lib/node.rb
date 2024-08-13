82be4fdca37c2f19120edd30e428587a1d4f682e
7minutech <elijohnson1107@gmail.com>
2024-08-13 16:56:39 -0500

    Added method number of children
    This method makes deletion of the binary tree
    easier b/c deletion changes depending on how
    many children a node has

--- lib/node.rb
+++ lib/node.rb
@@ -6,4 +6,13 @@ class Node
     @left_node = nil
     @right_node = nil
   end
+
+  def number_of_children
+    if @left_node && @right_node != nil
+      2
+    elsif @left_node != nil || @right_node != nil
+      1
+    else
+      0
+  end
 end
