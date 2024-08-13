8d1fc4a3c013b828e4c6db9503df10ed795b879f
7minutech <elijohnson1107@gmail.com>
2024-08-13 01:07:49 -0500

    Added build tree method

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,13 +1,19 @@
 require_relative "node"
 class Tree
-  attr_accessor :arr
+  attr_accessor :arr, :root
 
   def initialize(arr)
     @arr = arr
-    @tree_arr = nil
-    @root = node.new(arr[arr.length / 2])
+    @root = build_tree
   end
 
-  def build_tree
+  def build_tree(array = @arr, start = 0, last = @arr.length - 1)
+    return nil if start > last
+
+    mid = (start + last) / 2
+    root = Node.new(array[mid])
+    root.left_node = (build_tree(array, start, mid - 1))
+    root.right_node = (build_tree(array, mid + 1, last))
+    root
   end
 end
