3ffdb295dd23b3ee57238e8bc98376f928af2cfa
7minutech <elijohnson1107@gmail.com>
2024-08-15 16:52:51 -0500

    Added TOP testing

--- main.rb
+++ main.rb
@@ -1,6 +1,27 @@
 require_relative "lib/node"
 require_relative "lib/tree"
-arr = [1, 2, 3, 4, 5, 6, 7]
-my_tree = Tree.new(arr)
-my_tree.build_tree
-puts my_tree.tree_arr
+my_arr = (Array.new(15) { rand(1..100) })
+my_tree = Tree.new(my_arr)
+puts my_tree.pretty_print
+my_tree.level_order.each { |node| print "#{node.value} " }
+puts " "
+my_tree.in_order.each { |node| print "#{node.value} " }
+puts " "
+my_tree.pre_order.each { |node| print "#{node.value} " }
+puts " "
+my_tree.post_order.each { |node| print "#{node.value} " }
+puts " "
+puts " "
+nums_to_unbalance = (Array.new(5) { rand(100..200) })
+nums_to_unbalance.each { |num| my_tree.insert(num) }
+my_tree.pretty_print
+puts my_tree.balanced?
+my_tree.rebalance
+my_tree.pretty_print
+my_tree.level_order.each { |node| print "#{node.value} " }
+puts " "
+my_tree.in_order.each { |node| print "#{node.value} " }
+puts " "
+my_tree.pre_order.each { |node| print "#{node.value} " }
+puts " "
+my_tree.post_order.each { |node| print "#{node.value} " }
